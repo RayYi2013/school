a8d0a8131ddb18a093ebf8595e7da91ec20f23bc
Ray <yishucheng@gmail.com>
2014-06-20 00:05:57 -0700

    init

---
+++ js/app.js
@@ -0,0 +1,134 @@
+/**
+ * Created by ray on 2014-06-19.
+ */
+
+
+angular.module('school', [
+    'ui.router',
+    'ngMap'
+])
+
+    .run(
+    [          '$rootScope', '$state', '$stateParams',
+        function ($rootScope,   $state,   $stateParams) {
+
+            // It's very handy to add references to $state and $stateParams to the $rootScope
+            // so that you can access them from any scope within your applications.For example,
+            // <li ui-sref-active="active }"> will set the <li> // to active whenever
+            // 'contacts.list' or one of its decendents is active.
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
+    ]
+)
+
+    .config(
+    [          '$stateProvider', '$urlRouterProvider',
+        function ($stateProvider,   $urlRouterProvider) {
+
+            /////////////////////////////
+            // Redirects and Otherwise //
+            /////////////////////////////
+
+            // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
+            $urlRouterProvider
+
+                // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
+                .otherwise('/');
+
+
+            //////////////////////////
+            // State Configurations //
+            //////////////////////////
+
+            // Use $stateProvider to configure your states.
+            $stateProvider
+
+                .state("root", {
+
+                    url: "",
+                    abstract: true,
+                    views : {
+                        'header': {
+                            templateUrl: 'views/header.html'
+                        },
+                        'footer': {
+                            templateUrl: 'views/footer.html'
+                        }
+                    }
+
+                })
+                .state("home", {
+
+                    url: "/",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/main.html'
+                        }
+                    }
+
+                })
+                .state("advance1", {
+
+                    url: "/advance1",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/advance1.html'
+                        }
+                    }
+                })
+                .state("basc1", {
+
+                    url: "/basc1",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/basc1.html'
+                        }
+                    }
+                })
+                .state("about-us", {
+
+                    url: "/about-us",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/about-us.html'
+                        }
+                    }
+                })
+                .state("contact-us", {
+
+                    url: "/contact-us",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/contact-us.html'
+                        }
+                    }
+                })
+                .state("map", {
+
+                    url: "/map",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/map.html'
+                        }
+                    }
+                })
+                .state("faq", {
+
+                    url: "/faq",
+                    parent: 'root',
+                    views : {
+                        'main@': {
+                            templateUrl: 'views/faq.html'
+                        }
+                    }
+                });
+        }
+    ]
+);
